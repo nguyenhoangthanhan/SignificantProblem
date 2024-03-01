3dd597bc39d5f5c77dc3f22ebb4b30028a1e9af7
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2024-03-02 01:51:17 +0700

    Collision Detection

--- SignificantProblems/app/src/main/java/com/nguyenhoangthanhan/significantproblems/collision/CustomView.kt
+++ SignificantProblems/app/src/main/java/com/nguyenhoangthanhan/significantproblems/collision/CustomView.kt
@@ -3,21 +3,47 @@ package com.nguyenhoangthanhan.significantproblems.collision
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
 class CustomView(context: Context): View(context) {
 
+    val rect = RectF(
+        100f, // left
+        100f, // top
+        500f, // right
+        500f, // bottom
+    )
+
+    val paint = Paint().apply {
+        color = Color.BLACK
+        style = Paint.Style.STROKE
+        strokeWidth = 4f
+    }
+
+    var collision = false
+
     override fun onDraw(canvas: Canvas) {
-        canvas.drawColor(Color.GREEN)
+        canvas.apply {
+            if (collision){
+                drawColor(Color.RED)
+            }else {
+                drawColor(Color.GREEN)
+            }
+            drawRect(rect, paint)
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         event?.apply {
             when(action){
                 MotionEvent.ACTION_DOWN -> {
-                    Log.d("NHTA_COLLISION", "down at $x $y")
+                    collision = rect.contains(x, y)
+                    invalidate()
+                    Log.d("NHTA _COLLISION", "down at $x $y")
                     return true
                 }
                 MotionEvent.ACTION_MOVE -> {
